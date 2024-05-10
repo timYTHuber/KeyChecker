b9a1082b189702bbee1b50c26ada47d4e19d474c
Srfox <srfox12000@gmail.com>
2024-05-10 19:41:51 +1000

    ae

---
+++ script.js
@@ -0,0 +1,19 @@
+function button() {
+    var input = document.getElementById("input");
+    var inputValue = input.value;
+
+    if (inputValue === "SLX-QSFTMECXBUSQQNLU") {
+      var scriptLink = "https://raw.githubusercontent.com/timYTHuber/AllInOne/main/Hub.lua";
+      navigator.clipboard.writeText(scriptLink)
+        .then(function() {
+          alert("Link to script copied to clipboard!");
+        })
+        .catch(function() {
+          alert("Something going wrong...");
+        });
+    } 
+    else {
+        alert("You are absolutely wrong!");
+        setTimeout("window.location='https://fauux.neocities.org/statue/statue2'", 3000);
+    }
+  }
